4d982a6afc3c4294ff7a572ca82a3492893cb095
kusi <lawcubegsm@gmail.com>
2022-10-25 16:16:46 +0000

    Add project Sql commands
    - update table
    - repopulate the table
    - add queries for the question with screenshot

--- data.sql
+++ data.sql
@@ -12,3 +12,19 @@ INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES('Pikachu', '2021-01-7', 1, FALSE, 15.04);
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES('Devimon', '2017-05-12', 5, TRUE, 11);
+
+-- project 2
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES('Charmander', '2020-02-08',-11, FALSE, 0);
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES('Plantmon', '2021-11-15',-5.7, TRUE, 2);
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES('Squirtle', '1993-04-02',-12.13, FALSE, 3);
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES('Angemon', '2005-06-12',-45, TRUE, 1);
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES('Boarmon', '2005-06-07',20.4, TRUE, 7);
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES('Blossom', '1998-10-13',17, TRUE, 3);
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES('Ditto', '2022-05-14',22, TRUE, 4);
--- schema.sql
+++ schema.sql
@@ -1,8 +1,10 @@
 /* Database schema to keep the structure of entire database. */
+DROP TABLE IF EXISTS animals
 
 CREATE TABLE animals(
    id INT GENERATED ALWAYS AS IDENTITY,
    name    VARCHAR(100),
+   species VARCHAR(100),
    date_of_birth  DATE,
    escape_attempts INT,
    neutered  BOOL,
