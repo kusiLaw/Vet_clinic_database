66b2fdb1791fb6a943d37e03ef779f55aa4221e9
skyv26 <dev.aakashv@gmail.com>
2022-11-01 16:39:23 +0530

    Added auditing query for visits

--- schema.sql
+++ schema.sql
@@ -82,3 +82,4 @@ ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
 CREATE INDEX email_asc On owners(email ASC);
 CREATE INDEX animals_id_asc on visits(animals_id ASC);
+CREATE INDEX vet_id_indexing on visits(vet_id asc);
